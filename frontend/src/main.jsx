861d8c8ca2ec1c395325eaa4f3e8f5af84aff5e4
Darius Amenuvor <daarius24@gmail.com>
2025-11-23 10:27:44 -0600

    Add Discover page with search and filter functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Auth0Provider } from "@auth0/auth0-react";
-import Saved from "./pages/Saved.jsx";
-import Landing from "./pages/Landing.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App.jsx";
+import Landing from "./pages/Landing.jsx";
+import Discover from "./pages/Discover.jsx";
+import Saved from "./pages/Saved.jsx";
 import OrgDashboard from "./pages/OrgDashboard.jsx";
 import OpportunityDetails from "./pages/OpportunityDetails.jsx";
-import "./index.css";
 import AdminDashboard from "./pages/AdminDashboard.jsx";
 import Signup from "./pages/SignUp.jsx";
 import Auth from "./pages/Auth.jsx";
+import "./index.css";
 
-// <Route path="/" element={<App />} />
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
@@ -25,19 +24,16 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     >
       <BrowserRouter>
         <Routes>
-         
           <Route path="/" element={<Landing />} />
+          <Route path="/discover" element={<Discover />} />
+          <Route path="/opportunity/:id" element={<OpportunityDetails />} />
           <Route path="/saved" element={<Saved />} />
           <Route path="/org/dashboard" element={<OrgDashboard />} />
-          <Route path="/opportunity/:id" element={<OpportunityDetails />} />
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/auth" element={<Auth />} />
-
-
         </Routes>
       </BrowserRouter>
     </Auth0Provider>
   </React.StrictMode>
 );
-
